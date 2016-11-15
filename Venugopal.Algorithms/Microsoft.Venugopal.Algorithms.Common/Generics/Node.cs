6b6b4d2cdab7ebbcf2decd0d78d7f96bdc6a8303
Venugopal Latchupatula <venu.csed@gmail.com>
2016-11-15 13:15:52 -0800

    Node, NodeCollection Templates

---
+++ Venugopal.Algorithms/Microsoft.Venugopal.Algorithms.Common/Generics/Node.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.Venugopal.Algorithms.Common.Generics
+{
+    public class Node<T>
+    {
+        #region Private 
+
+        T nodeValue;
+
+        NodeCollection<T> nodeNeighbors;
+
+        #endregion
+
+        public Node() {}
+
+        public Node(T data):this(data, new NodeCollection<T>()){}
+
+        public Node(T data, NodeCollection<T> neighbors)
+        {
+            this.nodeValue = data;
+            this.nodeNeighbors = neighbors;
+        }
+
+        #region Properties
+        
+        public T Value
+        {
+            get { return nodeValue; }
+        }
+
+        #endregion
+    }
+}
---
+++ Venugopal.Algorithms/Microsoft.Venugopal.Algorithms.Common/Generics/NodeCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Venugopal.Algorithms.Common.Generics
+{
+    /// <summary>
+    /// Collection container that holds the set of nodes.
+    /// </summary>
+    /// <typeparam name="T">
+    /// If you want the right Contains operation for your Type, Derive from IEquatable<T>.
+    /// More details: http://stackoverflow.com/questions/3637984/what-does-collection-contains-use-to-check-for-existing-objects 
+    /// </typeparam>
+    public class NodeCollection<T> : Collection<Node<T>>
+    {
+        public NodeCollection() : base() {}
+
+        public bool ContainsNodeWithValue(T data)
+        {
+            bool contains = false;
+
+            Parallel.ForEach(base.Items, itemAccessWork =>
+            {
+                // you dont need lock while you set the value since there is no problem of Write Inconsistency!
+                if (itemAccessWork.Value.Equals(data))
+                    contains = true;
+                
+            });
+
+            return contains;
+        }
+    }
+}
